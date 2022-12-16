44f6782cc948929ea7e074fa9ac6952feff08540
gulezahranaqvi <gulezahrasyeda980@gmail.com>
2022-12-16 07:58:05 -0800

    week3

---
+++ headercolor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main()
+{
+system ("color 35");
+cout<< "  *****************************************                    "<<endl;
+cout<< "  *                                       *                    "<<endl;
+cout<< "  *    University Admission               *                    "<<endl;
+cout<< "  *    Management System                  *                    "<<endl;
+cout<< "  *                                       *                    "<<endl;
+cout<< "  *                                       *                    "<<endl;
+cout<< "  *****************************************                    "<<endl;
+}
